76421e1a01d66ef425c125b41bf2c769562d0df2
Matt Darlington <matthewsdarlington@gmail.com>
2023-09-06 09:52:57 -0600

    Initial commit; finish iter 2

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,23 @@
+require './lib/student'
+
 class Course
-  
+  attr_reader :name, :capacity, :students
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
+
+  def full?
+    @students.count == @capacity
+  end
+
+  def enroll(student)
+    if !full?
+      @students << student
+    # else
+    #   puts "Can't enroll this student. Course is full."
+    end
+  end
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,38 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do
-  
+  it 'exists' do
+    course = Course.new("Calculus", 2)
+    expect(course).to be_a(Course)
+  end
+
+  it 'has attributes' do
+    course = Course.new("Calculus", 2)
+    expect(course.name).to eq("Calculus")
+    expect(course.capacity).to eq(2)
+  end
+
+  it 'starts with no students' do
+    course = Course.new("Calculus", 2)
+    expect(course.students).to eq([])
+  end
+  it 'keeps track of whether it\'s full' do
+    course = Course.new("Calculus", 2)
+    expect(course.full?).to eq(false)
+  end
+  it 'can enroll students' do
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29}) 
+    course = Course.new("Calculus", 2)
+    course.enroll(student1)
+    course.enroll(student2)
+
+    expect(course.students).to eq([student1, student2])
+    expect(course.full?).to eq(true)
+
+    student3 = Student.new({name: "Kristen", age: 23}) 
+    course.enroll(student3)
+
+    expect(course.students).to eq([student1, student2])
+  end
 end
